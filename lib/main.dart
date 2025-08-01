4a2c29d6eae0d413e0dd8d24623bda8ac45593b5
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-08-02 03:45:57 +0500

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter_firebase/firebase_options.dart';
 import 'package:instagram_clone_flutter_firebase/responsive/layout_screen.dart';
 import 'package:instagram_clone_flutter_firebase/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone_flutter_firebase/responsive/web_screen_layout.dart';
 import 'package:instagram_clone_flutter_firebase/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
