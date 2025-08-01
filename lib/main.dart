c4945beb96d42e4c7272ab0ec65189b448e0589a
muhammadXainAhmad <206599128+muhammadXainAhmad@users.noreply.github.com>
2025-08-02 00:56:39 +0500

    Responsive layout for mobile & web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone_flutter_firebase/responsive/layout_screen.dart';
+import 'package:instagram_clone_flutter_firebase/responsive/mobile_screen_layout.dart';
+import 'package:instagram_clone_flutter_firebase/responsive/web_screen_layout.dart';
+import 'package:instagram_clone_flutter_firebase/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(body: Text("LETS BUILD INSTAGRAM")),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
+      ),
     );
   }
 }
